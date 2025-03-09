fb852986d12e791331bc3ac4d29bb3199ca32a0e
Hoshikute <oxuuxo@qq.com>
2025-03-10 00:47:51 +0800

    first commit

---
+++ Assets/ZMPackages/ZMGCFrameWork/Runtime/HallWorldScriptExecutionOrder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class HallWorldScriptExecutionOrder  :IBehaviourExecution
+{
+    private static Type[] LogicBehaviorExecutions = new Type[] {
+     };
+
+    private static Type[] DataBehaviorExecutions = new Type[] {
+     };
+
+    private static Type[] MsgBehaviorExecutions = new Type[] {
+     };
+
+    public Type[] GetDataBehaviourExecution()
+    {
+        return DataBehaviorExecutions;
+    }
+
+    public Type[] GetLogicBehaviourExecution()
+    {
+        return LogicBehaviorExecutions;
+    }
+
+    public Type[] GetMsgBehaviourExecution()
+    {
+        return MsgBehaviorExecutions;
+    }
+}
