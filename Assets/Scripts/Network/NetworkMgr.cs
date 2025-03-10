eb752c0a81d18d9277a606b59a8dcdafe8e96ed9
Hoshikute <oxuuxo@qq.com>
2025-03-11 01:53:40 +0800

    网络管理器

--- Assets/Scripts/Network/NetworkMgr.cs
+++ Assets/Scripts/Network/NetworkMgr.cs
@@ -5,23 +5,38 @@ using Fantasy;
 using ZM.ZMAsset;
 using Sirenix.OdinInspector.Editor.Drawers;
 using Fantasy.Async;
+using Fantasy.Network;
 
 public class NetworkMgr:Singleton<NetworkMgr>
 {
-    public static NetworkMgr instance;
-    public static NetworkMgr Instance{
-        get{
-            if(instance == null){
-                instance = new NetworkMgr();
-            }
-            return instance;
-        }
-    }
-
+    private Session mSession;
     private Scene mScene;
 
-    public async FTask<Scene> Init()
+    public async FTask InitSettings()
     {
-        return mScene = await Fantasy.Entry.Scene.LoadScene("Main");
+        await Fantasy.Platform.Unity.Entry.Initialize(GetType().Assembly);
+        mScene = await Scene.Create(SceneRuntimeType.MainThread);
+    }
+
+    public Session Connect()
+    {
+        mSession = mScene.Connect("127.0.0.1:2000",NetworkProtocolType.KCP,
+            OnConnectSuccess,OnConnectFail,OnDisConnected,true);
+        return mSession;
+    }
+
+    public void OnConnectSuccess()
+    {
+
+    }
+
+    public void OnConnectFail()
+    {
+
+    }
+
+    public void OnDisConnected()
+    {
+
     }
 }
